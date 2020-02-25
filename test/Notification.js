b45684e17a9f050617d0a17e3d1a96e9e4113e46
shishir75 <shishir.srdr16@gmail.com>
2020-02-25 22:08:51 +0600

    Refactor Test

--- test/Notification.js
+++ test/Notification.js
@@ -3,30 +3,20 @@ import test from "ava";
 import Notification from "../src/Notification";
 
 
-test('that it renders a notification',  t => {
+let vm;
 
+test.beforeEach(t => {
     let N = Vue.extend(Notification);
 
-    let vm = new N({ propsData : {
+    vm = new N({ propsData : {
 
             message: 'Foobar'
 
         } }).$mount();
-
-    t.is(vm.$el.textContent, 'FOOBAR');
-
 });
 
 
-test('that it capitalize the message',  t => {
-
-    let N = Vue.extend(Notification);
-
-    let vm = new N({ propsData : {
-
-            message: 'foobar'
-
-        } }).$mount();
+test('that it renders a notification',  t => {
 
     t.is(vm.$el.textContent, 'FOOBAR');
 
